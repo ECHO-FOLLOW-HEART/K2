9674af82dd83ceab72f9c5b2e83c26537749a37b
zephyre <haizi.zh@gmail.com>
2014-06-24 20:19:09 +0800

    ADD: 对景点进行搜索。

--- app/controllers/POICtrl.java
+++ app/controllers/POICtrl.java
@@ -10,6 +10,8 @@ import play.mvc.Result;
 import utils.Utils;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * POI相关信息。
@@ -17,6 +19,12 @@ import java.net.UnknownHostException;
  * @author Zephyre
  */
 public class POICtrl extends Controller {
+    /**
+     * 获得经典的详细信息。
+     *
+     * @param spotId 景点ID。
+     * @throws UnknownHostException
+     */
     public static Result getViewSpot(String spotId) throws UnknownHostException {
         MongoClient client = Utils.getMongoClient("localhost", 27017);
         DB db = client.getDB("view_spot");
@@ -34,5 +42,67 @@ public class POICtrl extends Controller {
         json.remove("_id");
         return Utils.createResponse(ErrorCode.NORMAL, json);
 
+    }
+
+    /**
+     * 搜索景点的ID。
+     *
+     * @param locality   景点所在地区的ID。
+     * @param tagFilter  按照标签对景点进行过滤。
+     * @param sortFilter 景点列表排序的字段，目前仅支持rating，即按照评分进行排序。
+     * @param sort       升序(asc)还是降序(desc)
+     * @param page       起始页码。
+     * @param pageSize   页面大小。
+     */
+    public static Result searchViewSpot(long locality, String tagFilter, String sortFilter, String sort,
+                                        int page, int pageSize) throws UnknownHostException {
+        MongoClient client = Utils.getMongoClient("localhost", 27017);
+        DB db = client.getDB("view_spot");
+        DBCollection col = db.getCollection("core");
+
+        BasicDBObject query = new BasicDBObject();
+        query.put("geo.locality.localityId", locality);
+        if (tagFilter != null && !tagFilter.isEmpty())
+            query.put("tags", tagFilter);
+        DBCursor cursor = col.find(query).skip(page * pageSize).limit(pageSize);
+
+        List<JsonNode> resultList = new ArrayList<>();
+        while (cursor.hasNext()) {
+            DBObject item = cursor.next();
+            ObjectNode node = Json.newObject();
+            node.put("spotId", item.get("_id").toString());
+
+            Object tmp = item.get("ratings");
+            if (tmp != null)
+                node.put("ratings", Json.parse(tmp.toString()));
+            tmp = item.get("tags");
+            if (tmp != null)
+                node.put("tags", Json.parse(tmp.toString()));
+
+            tmp = item.get("name");
+            if (tmp != null)
+                node.put("name", tmp.toString());
+
+            tmp = ((DBObject) item.get("introduction")).get("description");
+            if (tmp != null) {
+                String desc = tmp.toString();
+                // 只取前36个字符。
+                if (desc.length() > 36)
+                    desc = desc.substring(0, 36);
+                node.put("description", desc);
+            }
+
+            tmp = item.get("geo");
+            if (tmp != null)
+                node.put("geo", Json.parse(tmp.toString()));
+
+            tmp = item.get("imageList");
+            if (tmp != null)
+                node.put("imageList", Json.parse(tmp.toString()));
+
+            resultList.add(node);
+        }
+
+        return Utils.createResponse(ErrorCode.NORMAL, Json.toJson(resultList));
     }
 }
