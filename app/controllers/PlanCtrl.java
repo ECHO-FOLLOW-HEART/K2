343ebbf60b4bee6e42bc402c8ec55344c3579fac
Zephyre <haizi.zh@gmail.com>
2014-07-04 13:45:00 +0800

    ADD: 路线规划。

--- app/controllers/PlanCtrl.java
+++ app/controllers/PlanCtrl.java
@@ -3,6 +3,8 @@ package controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mongodb.*;
+import exception.ErrorCode;
+import exception.TravelPiException;
 import org.apache.commons.lang3.*;
 import org.bson.types.ObjectId;
 import play.libs.Json;
@@ -12,10 +14,7 @@ import utils.Planner;
 import utils.Utils;
 
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 
 /**
@@ -49,110 +48,123 @@ public class PlanCtrl extends Controller {
             backLoc = locCol.findOne(QueryBuilder.start("_id").is(new ObjectId(backLocId)).get());
             if (backLoc == null)
                 throw new NullPointerException();
+            backLocId = backLoc.get("_id").toString();
         } catch (IllegalArgumentException | NullPointerException e) {
             return Utils.createResponse(ErrorCode.INVALID_ARGUMENT, String.format("Invalid plan ID: %s.", planId));
         }
 
-        // 获得基准时间
+        // 获得基准时间（默认为3天后）
+        Calendar calBase = Calendar.getInstance();
+        calBase.add(Calendar.DAY_OF_YEAR, 3);
+        calBase.set(Calendar.HOUR, 0);
+        calBase.set(Calendar.MINUTE, 0);
+        calBase.set(Calendar.SECOND, 0);
+        calBase.set(Calendar.MILLISECOND, 0);
 
-        ObjectNode ret = Json.newObject();
-        ret.put("_id", plan.get("_id").toString());
-        DBObject loc = (DBObject) plan.get("loc");
-        ret.put("loc", Json.toJson(BasicDBObjectBuilder.start("_id", loc.get("_id").toString())
-                .add("name", loc.get("name").toString()).get()));
+        DBObject ret1 = Planner.generateUgcPlan(planId, fromLocId, backLocId, calBase);
 
-        for (String key : new String[]{"target", "title", "tags", "days", "desc", "imageList", "viewCnt"}) {
-            Object tmp = plan.get(key);
-            if (tmp != null)
-                ret.put(key, Json.toJson(tmp));
-        }
+        return Utils.createResponse(ErrorCode.NORMAL, Utils.bsonToJson(ret1));
 
-        // 获取路线详情
-        BasicDBList detailsList = (BasicDBList) plan.get("details");
-        // 整个路线详情列表
-        List<JsonNode> detailNodes = new ArrayList<>();
-
-        if (detailsList != null) {
-            int curDay = -1;
-            // 单日路线详情列表（只处理景区）
-            List<JsonNode> detailNodesD = new ArrayList<>();
-
-            // 按照dayIdx和idx的顺序进行排序
-            Collections.sort(detailsList, new Comparator<Object>() {
-                @Override
-                public int compare(Object o1, Object o2) {
-                    int dayIdx1 = (int) ((DBObject) o1).get("dayIdx");
-                    int dayIdx2 = (int) ((DBObject) o2).get("dayIdx");
-                    int idx1 = (int) ((DBObject) o1).get("idx");
-                    int idx2 = (int) ((DBObject) o2).get("idx");
-
-                    int c = dayIdx1 - dayIdx2;
-                    if (c == 0)
-                        c = idx1 - idx2;
-                    return c;
-                }
-            });
-
-            DBCollection vsCol = Utils.getMongoClient().getDB("poi").getCollection("view_spot");
-
-            for (Object aDetailsList : detailsList) {
-                DBObject detailsItem = (DBObject) aDetailsList;
-                DBObject item = (DBObject) detailsItem.get("item");
-                if (!item.get("type").equals("vs"))
-                    continue;
-
-                ObjectNode node = Json.newObject();
-
-                int dayIdx = (int) detailsItem.get("dayIdx");
-                node.put("dayIdx", dayIdx);
-
-                // 景点
-                ObjectNode vsNode = Json.newObject();
-                vsNode.put("_id", item.get("_id").toString());
-                vsNode.put("name", item.get("name").toString());
-                vsNode.put("type", "vs");
-                DBObject vs = vsCol.findOne(QueryBuilder.start("_id").is(item.get("_id")).get());
-                Object tmp = vs.get("tags");
-                if (tmp != null)
-                    vsNode.put("tags", Json.toJson(tmp));
-                tmp = vs.get("intro");
-                if (tmp != null) {
-                    tmp = ((DBObject) tmp).get("desc");
-                    if (tmp != null)
-                        vsNode.put("desc", StringUtils.abbreviate(tmp.toString(), 64));
-                }
-                node.put("item", vsNode);
-
-                DBObject stopLoc = (DBObject) detailsItem.get("loc");
-                ObjectNode locNode = Json.newObject();
-                locNode.put("_id", stopLoc.get("_id").toString());
-                locNode.put("name", stopLoc.get("name").toString());
-                node.put("loc", locNode);
-
-                if (dayIdx != curDay) {
-                    if (!detailNodesD.isEmpty())
-                        detailNodes.add(Json.toJson(detailNodesD));
-                    detailNodesD = new ArrayList<>();
-                }
-                detailNodesD.add(node);
-                curDay = dayIdx;
-            }
-            if (!detailNodesD.isEmpty())
-                detailNodes.add(Json.toJson(detailNodesD));
-        }
-
-        if (traffic != 0) {
-            // TODO 交通的起止时间，需要根据当天的游玩景点而定。
-            // 添加大交通
-            Planner.telomere(detailNodes, fromLoc, true);
-            Planner.telomere(detailNodes, backLoc, false);
-        }
-
-        // 添加每晚住宿
-
-        ret.put("details", Json.toJson(detailNodes));
-
-        return Utils.createResponse(ErrorCode.NORMAL, ret);
+//        ObjectNode ret = Json.newObject();
+//        ret.put("_id", plan.get("_id").toString());
+//        DBObject loc = (DBObject) plan.get("loc");
+//        ret.put("loc", Json.toJson(BasicDBObjectBuilder.start("_id", loc.get("_id").toString())
+//                .add("name", loc.get("name").toString()).get()));
+//
+//        for (String key : new String[]{"target", "title", "tags", "days", "desc", "imageList", "viewCnt"}) {
+//            Object tmp = plan.get(key);
+//            if (tmp != null)
+//                ret.put(key, Json.toJson(tmp));
+//        }
+//
+//        // 获取路线详情
+//        BasicDBList detailsList = (BasicDBList) plan.get("details");
+//
+//
+//        // 整个路线详情列表
+//        List<JsonNode> detailNodes = new ArrayList<>();
+//
+//        if (detailsList != null) {
+//            int curDay = -1;
+//            // 单日路线详情列表（只处理景区）
+//            List<JsonNode> detailNodesD = new ArrayList<>();
+//
+//            // 按照dayIdx和idx的顺序进行排序
+//            Collections.sort(detailsList, new Comparator<Object>() {
+//                @Override
+//                public int compare(Object o1, Object o2) {
+//                    int dayIdx1 = (int) ((DBObject) o1).get("dayIdx");
+//                    int dayIdx2 = (int) ((DBObject) o2).get("dayIdx");
+//                    int idx1 = (int) ((DBObject) o1).get("idx");
+//                    int idx2 = (int) ((DBObject) o2).get("idx");
+//
+//                    int c = dayIdx1 - dayIdx2;
+//                    if (c == 0)
+//                        c = idx1 - idx2;
+//                    return c;
+//                }
+//            });
+//
+//            DBCollection vsCol = Utils.getMongoClient().getDB("poi").getCollection("view_spot");
+//
+//            for (Object aDetailsList : detailsList) {
+//                DBObject detailsItem = (DBObject) aDetailsList;
+//                DBObject item = (DBObject) detailsItem.get("item");
+//                if (!item.get("type").equals("vs"))
+//                    continue;
+//
+//                ObjectNode node = Json.newObject();
+//
+//                int dayIdx = (int) detailsItem.get("dayIdx");
+//                node.put("dayIdx", dayIdx);
+//
+//                // 景点
+//                ObjectNode vsNode = Json.newObject();
+//                vsNode.put("_id", item.get("_id").toString());
+//                vsNode.put("name", item.get("name").toString());
+//                vsNode.put("type", "vs");
+//                DBObject vs = vsCol.findOne(QueryBuilder.start("_id").is(item.get("_id")).get());
+//                Object tmp = vs.get("tags");
+//                if (tmp != null)
+//                    vsNode.put("tags", Json.toJson(tmp));
+//                tmp = vs.get("intro");
+//                if (tmp != null) {
+//                    tmp = ((DBObject) tmp).get("desc");
+//                    if (tmp != null)
+//                        vsNode.put("desc", StringUtils.abbreviate(tmp.toString(), 64));
+//                }
+//                node.put("item", vsNode);
+//
+//                DBObject stopLoc = (DBObject) detailsItem.get("loc");
+//                ObjectNode locNode = Json.newObject();
+//                locNode.put("_id", stopLoc.get("_id").toString());
+//                locNode.put("name", stopLoc.get("name").toString());
+//                node.put("loc", locNode);
+//
+//                if (dayIdx != curDay) {
+//                    if (!detailNodesD.isEmpty())
+//                        detailNodes.add(Json.toJson(detailNodesD));
+//                    detailNodesD = new ArrayList<>();
+//                }
+//                detailNodesD.add(node);
+//                curDay = dayIdx;
+//            }
+//            if (!detailNodesD.isEmpty())
+//                detailNodes.add(Json.toJson(detailNodesD));
+//        }
+//
+////        if (traffic != 0) {
+////            // TODO 交通的起止时间，需要根据当天的游玩景点而定。
+////            // 添加大交通
+////            Planner.telomere(detailsList, fromLoc, calBase, true);
+////            Planner.telomere(detailsList, backLoc, calBase, false);
+////        }
+//
+//        // 添加每晚住宿
+//
+//        ret.put("details", Json.toJson(detailNodes));
+//
+//        return Utils.createResponse(ErrorCode.NORMAL, ret);
     }
 
     /**
--- app/utils/Planner.java
+++ app/utils/Planner.java
@@ -2,17 +2,17 @@ package utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.mongodb.BasicDBList;
-import com.mongodb.DBObject;
-import controllers.TravelPiException;
+import com.mongodb.*;
+import exception.ErrorCode;
+import exception.TravelPiException;
+import org.bson.types.ObjectId;
+import play.filters.csrf.CSRF;
 import play.libs.Json;
 
+import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.TimeZone;
+import java.util.*;
 
 /**
  * 和路线规划相关的静态类
@@ -22,13 +22,214 @@ import java.util.TimeZone;
  */
 public class Planner {
 
+    public static DBObject generateUgcPlan(String planId, String fromLocId, String backLocId, Calendar baseDate)
+            throws UnknownHostException, TravelPiException {
+        DBCollection col = Utils.getMongoClient().getDB("plan").getCollection("plan_info");
+        DBCollection locCol = Utils.getMongoClient().getDB("geo").getCollection("locality");
+        DBCollection hotelCol = Utils.getMongoClient().getDB("poi").getCollection("hotel");
+
+        DBObject plan, fromLoc, backLoc;
+        try {
+            plan = col.findOne(QueryBuilder.start("_id").is(new ObjectId(planId)).get());
+            if (plan == null)
+                throw new NullPointerException();
+            fromLoc = locCol.findOne(QueryBuilder.start("_id").is(new ObjectId(fromLocId)).get());
+            if (fromLoc == null)
+                throw new NullPointerException();
+            if (backLocId == null || backLocId.isEmpty())
+                backLocId = fromLocId;
+            backLoc = locCol.findOne(QueryBuilder.start("_id").is(new ObjectId(backLocId)).get());
+            if (backLoc == null)
+                throw new NullPointerException();
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new TravelPiException(ErrorCode.INVALID_ARGUMENT, String.format("Invalid plan ID: %s.", planId), e);
+        }
+
+        BasicDBList details = (BasicDBList) plan.get("details");
+
+        if (details == null || details.size() == 0)
+            return plan;
+
+        // 如果出发地和目的地不在一个城市，并且没有大交通信息，则添加出发端粒
+        // TODO 判断出发地和目的地是否为一个城市
+        BasicDBList activities = (BasicDBList) ((DBObject) details.get(0)).get("actv");
+        DBObject actv = (DBObject) activities.get(0);
+        if (!actv.get("type").equals("majorTraffic"))
+            telomere(details, fromLoc, baseDate, true);
+
+        // 进行日期标注
+        Calendar curDate = Calendar.getInstance(Utils.getDefaultTimeZone());
+        curDate.setTimeInMillis(baseDate.getTimeInMillis());
+        for (Object obj : details) {
+            DBObject dayNode = (DBObject) obj;
+            if (!dayNode.containsField("date"))
+                dayNode.put("date", curDate);
+
+            Calendar newCurDate = Calendar.getInstance(Utils.getDefaultTimeZone());
+            newCurDate.setTimeInMillis(curDate.getTimeInMillis());
+            curDate = newCurDate;
+            curDate.add(Calendar.DAY_OF_YEAR, 1);
+        }
+
+        activities = (BasicDBList) ((DBObject) details.get(details.size() - 1)).get("actv");
+        actv = (DBObject) activities.get(activities.size() - 1);
+        if (!actv.get("type").equals("majorTraffic")) {
+            Calendar lastCal = Calendar.getInstance(Utils.getDefaultTimeZone());
+            lastCal.setTimeInMillis(((Calendar) ((DBObject) details.get(details.size() - 1)).get("date")).getTimeInMillis());
+            lastCal.add(Calendar.DAY_OF_YEAR, 1);
+            telomere(details, fromLoc, lastCal, false);
+        }
+
+        // 处理住宿
+        for (int i = 0; i < details.size() - 1; i++) {
+            DBObject dayNode = (DBObject) details.get(i);
+            activities = (BasicDBList) dayNode.get("actv");
+            actv = (DBObject) activities.get(activities.size() - 1);
+            if (actv.get("type").equals("hotel"))
+                continue;
+
+            ObjectId locId;
+            String locName;
+            if (actv.get("type").equals("majorTraffic")) {
+                locId = (ObjectId) actv.get("arrLocId");
+                locName = actv.get("arrLocName").toString();
+            } else {
+
+                locId = (ObjectId) actv.get("locId");
+                locName = actv.get("locName").toString();
+            }
+
+            DBCursor cursor = hotelCol.find(QueryBuilder.start("geo.locality._id").is(locId).get())
+                    .sort(BasicDBObjectBuilder.start("ratings.score", -1).get())
+                    .limit(1);
+            if (!cursor.hasNext())
+                continue;
+            DBObject hotel = cursor.next();
+
+            DBObject hotelActv = BasicDBObjectBuilder.start().add("itemId", hotel.get("_id"))
+                    .add("itemName", hotel.get("name"))
+                    .add("locId", locId)
+                    .add("locName", locName)
+                    .add("type", "hotel").get();
+            activities.add(hotelActv);
+        }
+
+        return plan;
+    }
+
+
     /**
      * 为路线添加大交通信息
      *
      * @param detailNodes
      * @param fromLoc
      */
-    public static void telomere(List<JsonNode> detailNodes, DBObject fromLoc, boolean start) throws TravelPiException {
+    private static void telomere(BasicDBList detailNodes, DBObject fromLoc, Calendar baseDate, boolean start) throws TravelPiException {
+        if (detailNodes == null || detailNodes.size() == 0)
+            return;
+
+        String depLocId, arrLocId;
+        List<Calendar> epTimeLimits;
+        if (start) {
+            // 出发时间限制
+            Calendar depCal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Shanghai"));
+            depCal.setTimeInMillis(baseDate.getTimeInMillis());
+            depCal.set(Calendar.HOUR, 6);
+            depCal.set(Calendar.MINUTE, 0);
+            depCal.set(Calendar.SECOND, 0);
+            depCal.set(Calendar.MILLISECOND, 0);
+
+            Calendar arrCal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Shanghai"));
+            arrCal.setTimeInMillis(depCal.getTimeInMillis());
+            arrCal.add(Calendar.DAY_OF_YEAR, 1);
+            arrCal.set(Calendar.HOUR, 11);
+
+            epTimeLimits = new ArrayList<>();
+            epTimeLimits.add(depCal);
+            epTimeLimits.add(arrCal);
+            depLocId = fromLoc.get("_id").toString();
+
+            BasicDBList activities = (BasicDBList) ((DBObject) detailNodes.get(0)).get("actv");
+            arrLocId = ((DBObject) activities.get(0)).get("locId").toString();
+        } else {
+            // 返回时间限制
+            Calendar depCal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Shanghai"));
+            depCal.setTimeInMillis(baseDate.getTimeInMillis());
+            depCal.set(Calendar.HOUR, 14);
+            depCal.set(Calendar.MINUTE, 0);
+            depCal.set(Calendar.SECOND, 0);
+            depCal.set(Calendar.MILLISECOND, 0);
+
+            Calendar arrCal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Shanghai"));
+            arrCal.setTimeInMillis(depCal.getTimeInMillis());
+            arrCal.add(Calendar.DAY_OF_YEAR, 1);
+            arrCal.set(Calendar.HOUR, 23);
+
+            epTimeLimits = new ArrayList<>();
+            epTimeLimits.add(depCal);
+            epTimeLimits.add(arrCal);
+
+            BasicDBList activities = (BasicDBList) ((DBObject) detailNodes.get(detailNodes.size() - 1)).get("actv");
+            depLocId = ((DBObject) activities.get(activities.size() - 1)).get("locId").toString();
+            arrLocId = fromLoc.get("_id").toString();
+        }
+
+        // 查询列车信息
+        BasicDBList routeList = Traffic.searchTrainRoute(depLocId, arrLocId, null,
+                null, null, epTimeLimits, null, Traffic.SortField.TIME_COST, Traffic.SortType.ASC);
+        if (routeList.size() > 0) {
+            DBObject route = (DBObject) routeList.get(0);
+
+            // 是否需要单独分配一天
+            // TODO 目前一律需要
+            final DBObject actv = BasicDBObjectBuilder.start().add("itemId", route.get("_id"))
+                    .add("depStopId", ((DBObject) route.get("dep")).get("stopId"))
+                    .add("depStopName", ((DBObject) route.get("dep")).get("stopName"))
+                    .add("depLocId", ((DBObject) route.get("dep")).get("locId"))
+                    .add("depLocName", ((DBObject) route.get("dep")).get("locName"))
+                    .add("arrStopId", ((DBObject) route.get("arr")).get("stopId"))
+                    .add("arrStopName", ((DBObject) route.get("arr")).get("stopName"))
+                    .add("arrLocId", ((DBObject) route.get("arr")).get("locId"))
+                    .add("arrLocName", ((DBObject) route.get("arr")).get("locName"))
+                    .add("type", "majorTraffic")
+                    .add("subType", "train").get();
+            Date depTime = (Date) route.get("depTime");
+            Date arrTime = (Date) route.get("arrTime");
+            long duration = arrTime.getTime() - depTime.getTime();
+            Calendar depCal = Calendar.getInstance(Utils.getDefaultTimeZone());
+            depCal.setTime(depTime);
+            depCal.set(Calendar.YEAR, baseDate.get(Calendar.YEAR));
+            depCal.set(Calendar.DAY_OF_YEAR, baseDate.get(Calendar.DAY_OF_YEAR));
+            Calendar arrCal = Calendar.getInstance(Utils.getDefaultTimeZone());
+            arrCal.setTimeInMillis(depCal.getTimeInMillis() + duration);
+
+            DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
+            fmt.setTimeZone(Utils.getDefaultTimeZone());
+
+            actv.put("depTime", fmt.format(depCal.getTime()));
+            actv.put("arrTime", fmt.format(arrCal.getTime()));
+
+            DBObject dayNode = BasicDBObjectBuilder.start()
+                    .add("date", baseDate)
+                    .add("actv", new BasicDBList() {
+                        {
+                            add(actv);
+                        }
+                    }).get();
+            if (start)
+                detailNodes.add(0, dayNode);
+            else
+                detailNodes.add(dayNode);
+        }
+    }
+
+    /**
+     * 为路线添加大交通信息
+     *
+     * @param detailNodes
+     * @param fromLoc
+     */
+    public static void telomereOld(List<JsonNode> detailNodes, DBObject fromLoc, boolean start) throws TravelPiException {
         if (detailNodes == null || detailNodes.size() == 0)
             return;
 
