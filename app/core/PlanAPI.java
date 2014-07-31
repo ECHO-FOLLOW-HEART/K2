742a7b0181f1f55a5875e0f963a77dba9d4db98a
Zephyre <haizi.zh@gmail.com>
2014-07-31 17:05:29 +0800

    MOD: plan utils.

--- app/core/PlanAPI.java
+++ app/core/PlanAPI.java
@@ -200,57 +200,68 @@ public class PlanAPI {
             }
         }
 
-        // 取出景点安排最多的一天。如果比其前后两天多，则昀一下
-        int[] vsCnt = new int[entryList.size()];
-        for (int i = 0; i < entryList.size(); i++)
-            vsCnt[i] = entryList.get(i).actv.size();
+        int iterCnt = 0;
+        boolean modified = true;
+        while (iterCnt < 10 && modified) {
+            iterCnt++;
+            modified = false;
 
-        int idxMax = -1;
-        int max = 0;
-        for (int i = 0; i < vsCnt.length; i++) {
-            if (vsCnt[i] > max) {
-                idxMax = i;
-                max = vsCnt[i];
-            }
-        }
+            // 取出景点安排最多的一天。如果比其前后两天多，则昀一下
+            int[] vsCnt = new int[entryList.size()];
+            for (int i = 0; i < entryList.size(); i++)
+                vsCnt[i] = entryList.get(i).actv.size();
 
-        if (idxMax >= 0) {
-            // 是否比旁边的景点多2？
-            boolean shift = false;
-            int from = 0;
-            int to = 0;
-            if (idxMax == 0 && vsCnt[0] - vsCnt[1] > 2) {
-                shift = true;
-                from = 0;
-                to = 1;
-            } else if (idxMax == vsCnt.length - 1 && vsCnt[vsCnt.length - 1] - vsCnt[vsCnt.length - 2] > 2) {
-                shift = true;
-                from = vsCnt.length - 1;
-                to = vsCnt.length - 2;
-
-            } else if (vsCnt[idxMax] - vsCnt[idxMax - 1] > 2) {
-                shift = true;
-                from = idxMax;
-                to = idxMax - 1;
-            } else if (vsCnt[idxMax] - vsCnt[idxMax + 1] > 2) {
-                shift = true;
-                from = idxMax;
-                to = idxMax + 1;
-            }
-            if (shift) {
-                // 取出最后一个景点
-                PlanDayEntry entryFrom = entryList.get(from);
-                PlanDayEntry entryTo = entryList.get(to);
-
-                for (int i = entryFrom.actv.size() - 1; i >= 0; i--) {
-                    PlanItem item = entryFrom.actv.get(i);
-                    if (item.type.equals("vs")) {
-                        entryFrom.actv.remove(i);
-                        entryTo.actv.add(0, item);
-                        break;
-                    }
+            int idxMax = -1;
+            int max = 0;
+            for (int i = 0; i < vsCnt.length; i++) {
+                if (vsCnt[i] > max) {
+                    idxMax = i;
+                    max = vsCnt[i];
                 }
+            }
 
+            if (idxMax >= 0) {
+                // 是否比旁边的景点多2？
+                boolean shift = false;
+                int from = 0;
+                int to = 0;
+                if (idxMax == 0) {
+                    if (vsCnt[0] - vsCnt[1] > 2) {
+                        shift = true;
+                        from = 0;
+                        to = 1;
+                    }
+                } else if (idxMax == vsCnt.length - 1) {
+                    if (vsCnt[vsCnt.length - 1] - vsCnt[vsCnt.length - 2] > 2) {
+                        shift = true;
+                        from = vsCnt.length - 1;
+                        to = vsCnt.length - 2;
+                    }
+                } else if (vsCnt[idxMax] - vsCnt[idxMax - 1] > 2) {
+                    shift = true;
+                    from = idxMax;
+                    to = idxMax - 1;
+                } else if (vsCnt[idxMax] - vsCnt[idxMax + 1] > 2) {
+                    shift = true;
+                    from = idxMax;
+                    to = idxMax + 1;
+                }
+                if (shift) {
+                    modified = true;
+                    // 取出最后一个景点
+                    PlanDayEntry entryFrom = entryList.get(from);
+                    PlanDayEntry entryTo = entryList.get(to);
+
+                    for (int i = entryFrom.actv.size() - 1; i >= 0; i--) {
+                        PlanItem item = entryFrom.actv.get(i);
+                        if (item.type.equals("vs")) {
+                            entryFrom.actv.remove(i);
+                            entryTo.actv.add(0, item);
+                            break;
+                        }
+                    }
+
+                }
             }
         }
     }
