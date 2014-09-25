3c2b6705f8d8f36850a6f98e4e352a2ba9c19258
linchuan <linchuan@lvxingpai.cn>
2014-09-25 12:45:10 +0800

    MOD：Web推荐接口。去掉imageList

--- app/models/morphia/misc/Recommendation.java
+++ app/models/morphia/misc/Recommendation.java
@@ -81,33 +81,6 @@ public class Recommendation extends TravelPiBaseItem implements ITravelPiFormatt
 
         // 如果存在更高阶的images字段，则使用之
         if (images != null && !images.isEmpty()) {
-            List<ImageItem> imgList = new ArrayList<>();
-            for (ImageItem img : images) {
-                if (img.enabled != null && !img.enabled)
-                    continue;
-                imgList.add(img);
-            }
-
-            Collections.sort(imgList, new Comparator<ImageItem>() {
-                @Override
-                public int compare(ImageItem o1, ImageItem o2) {
-                    if (o1.fSize != null && o2.fSize != null)
-                        return o2.fSize - o1.fSize;
-                    else if (o1.w != null && o2.w != null)
-                        return o2.w - o1.w;
-                    else if (o1.h != null && o2.h != null)
-                        return o2.h - o1.h;
-                    else
-                        return 0;
-                }
-            });
-
-            List<String> ret = new ArrayList<>();
-            for (ImageItem img : imgList.subList(0, imgList.size() >= 5 ? 5 : imgList.size())) {
-                if (img.url != null)
-                    ret.add(img.url);
-            }
-            builder.add("imageList", ret);
 
             if (images != null) {
                 ArrayList<JsonNode> tmpList = new ArrayList<>();
@@ -116,11 +89,6 @@ public class Recommendation extends TravelPiBaseItem implements ITravelPiFormatt
                 builder.add("image", tmpList);
             }
 
-        } else if (imageList != null) {
-            ArrayList<String> tmpList = new ArrayList<>();
-            for (String img : imageList.subList(0, (imageList.size() >= 5 ? 5 : imageList.size())))
-                tmpList.add(img);
-            builder.add("imageList", tmpList);
         }
         return Json.toJson(builder.get());
     }
