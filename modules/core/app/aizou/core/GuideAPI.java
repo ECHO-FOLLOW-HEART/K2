bfd3060b8e49bfeee3b31268bf73890c3e28eaad
linchuan <linchuan@lvxingpai.cn>
2014-11-07 16:23:20 +0800

    ADD:添加保存攻略中行程单接口

--- modules/core/app/aizou/core/GuideAPI.java
+++ modules/core/app/aizou/core/GuideAPI.java
@@ -1,22 +1,51 @@
 package aizou.core;
 
+import exception.ErrorCode;
 import exception.TravelPiException;
-import javassist.bytecode.Descriptor;
 import models.MorphiaFactory;
 import models.guide.Guide;
-import models.poi.AbstractPOI;
+import models.guide.ItinerItem;
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.query.Query;
-import play.mvc.Result;
 
-import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by topy on 2014/11/5.
  */
 public class GuideAPI {
 
+    /**
+     * 根据ID取得攻略
+     *
+     * @param id
+     * @return
+     * @throws TravelPiException
+     */
+    public static Guide getGuideById(ObjectId id) throws TravelPiException {
+        if (id == null)
+            throw new TravelPiException(ErrorCode.INVALID_ARGUMENT, "Guide ID is null.");
+
+        Datastore ds = MorphiaFactory.getInstance().getDatastore(MorphiaFactory.DBType.GUIDE);
+        Query<Guide> query = ds.createQuery(Guide.class);
+        return query.field("_id").equal(id).get();
+    }
+
+    /**
+     * 保存行程单
+     *
+     * @param guide
+     * @param itemBeanList
+     * @throws TravelPiException
+     */
+    public static void saveItinerary(Guide guide, List<ItinerItem> itemBeanList) throws TravelPiException {
+        if (guide == null || itemBeanList == null || itemBeanList.isEmpty())
+            throw new TravelPiException(ErrorCode.INVALID_ARGUMENT, "Null guide cannot save.");
+        guide.itinerary = itemBeanList;
+        Datastore ds = MorphiaFactory.getInstance().getDatastore(MorphiaFactory.DBType.GUIDE);
+        ds.save(guide);
+    }
 
 
 }
--- modules/taozi/app/controllers/taozi/GuideCtrl.java
+++ modules/taozi/app/controllers/taozi/GuideCtrl.java
@@ -1,72 +1,91 @@
 package controllers.taozi;
 
+import aizou.core.GuideAPI;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import exception.ErrorCode;
+import exception.TravelPiException;
+import models.guide.Guide;
 import models.guide.ItinerItem;
 import models.poi.AbstractPOI;
 import models.poi.ViewSpot;
-import play.mvc.Controller;
 import org.bson.types.ObjectId;
+import play.mvc.Controller;
 import play.mvc.Result;
 import utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by topy on 2014/11/5.
  */
 public class GuideCtrl extends Controller {
 
-//    public static Result getTemplateGuide(String locId) {
-//        try {
-//                ObjectId locObjid = new ObjectId(locId);
-//
-//
-//
-//        } catch (TravelPiException e) {
-//            return Utils.createResponse(e.errCode, e.getMessage());
-//        }
-//    }
 
-    public static Result saveItinerary() {
+    /**
+     * 更新攻略中行程单内容
+     *
+     * @param id 攻略ID
+     * @return
+     */
+    public static Result updateItinerary(String id) {
 
         JsonNode data = request().body().asJson();
-
         ItinerItem itemBean;
-        JsonNode itineraries = data.get("itinerary");
-        if (null != itineraries && itineraries.isArray() && itineraries.findValues("itinerary") != null) {
+        List<ItinerItem> itemBeanList = new ArrayList<>();
+        try {
+            ObjectId guideId = new ObjectId(id);
+            Guide guide = GuideAPI.getGuideById(guideId);
+            JsonNode itineraries = data.get("itinerary");
+            if (itineraries == null)
+                throw new TravelPiException(ErrorCode.INVALID_ARGUMENT, "Put data doesn't contain itinerary.");
             for (JsonNode node : itineraries) {
-
-
+                itemBean = getItemFromJson(node);
+                itemBeanList.add(itemBean);
             }
+            //保存攻略
+            GuideAPI.saveItinerary(guide, itemBeanList);
+
+        } catch (NullPointerException e1) {
+            return Utils.createResponse(ErrorCode.DATA_NOT_EXIST, "NullPointerException happened.");
+        } catch (TravelPiException e) {
+            return Utils.createResponse(e.errCode, e.getMessage());
         }
-
-
-        return Utils.createResponse(ErrorCode.NORMAL, "");
+        return Utils.createResponse(ErrorCode.NORMAL, "Success.");
     }
 
-//    private static ItinerItem transJsonToItem(JsonNode node) throws NullPointerException {
-//        String type;
-//        AbstractPOI poiBean;
-//        ItinerItem itemBean = new ItinerItem();
-//        ObjectNode itemObject = (ObjectNode) node;
-//
-//        itemBean.dayIndex = itemObject.get("dayIdx").asInt();
-//        type = itemObject.get("type").asText();
-//        itemBean.type = type;
-//        switch (type) {
-//            case "vs":
-//                poiBean = new ViewSpot();
-//                break;
-//        }
-//        ObjectNode poiObject = (ObjectNode) itemObject.get("poi");
-//        String poiId = poiObject.get("_id").asText();
-//        poiBean.id = new ObjectId(poiId);
-//        poiBean.name = poiObject.get("zhName").asText();
-//        poiBean.enName = poiObject.get("enName").asText();
-//        poiBean.desc = poiObject.get("desc").asText();
-//        poiBean.cover = poiObject.get("cover").asText();
-//        itemBean.poi = poiBean;
-//        return itemBean;
-//
-//    }
+    /**
+     * 读取Json中的数据生成Bean
+     *
+     * @param node PUT数据
+     * @return
+     * @throws NullPointerException
+     * @throws TravelPiException
+     */
+    private static ItinerItem getItemFromJson(JsonNode node) throws NullPointerException, TravelPiException {
+        String type;
+        AbstractPOI poiBean;
+        ItinerItem itemBean = new ItinerItem();
+        ObjectNode itemObject = (ObjectNode) node;
+        itemBean.dayIndex = itemObject.get("dayIdx").asInt();
+        type = itemObject.get("type").asText();
+        itemBean.type = type;
+        switch (type) {
+            case "vs":
+                poiBean = new ViewSpot();
+                break;
+            default:
+                throw new TravelPiException(ErrorCode.INVALID_ARGUMENT, String.format("Error poi type."));
+        }
+        ObjectNode poiObject = (ObjectNode) itemObject.get("poi");
+        String poiId = poiObject.get("_id").asText();
+        poiBean.id = new ObjectId(poiId);
+        poiBean.name = poiObject.get("zhName").asText();
+        poiBean.enName = poiObject.get("enName").asText();
+        poiBean.desc = poiObject.get("desc").asText();
+        poiBean.cover = poiObject.get("cover").asText();
+        itemBean.poi = poiBean;
+        return itemBean;
+    }
 }
