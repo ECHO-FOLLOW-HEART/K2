2447e009599e6594065702beaf4e856d2dd6f450
刘晓飞 <lxf46466290@163.com>
2014-11-14 14:15:43 +0800

    修改poi评价接口

---
+++ modules/core/app/models/misc/TravelColumns.java
@@ -0,0 +1,43 @@
+package models.misc;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import models.TravelPiBaseItem;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Transient;
+
+/**
+ * Created by lxf on 14-11-12.
+ */
+@JsonFilter("travelColumnsFilter")
+@Entity
+public class TravelColumns extends TravelPiBaseItem{
+
+    @Transient
+    public static String fnCover="cover";
+    @Transient
+    public static String fnLink="link";
+    /**
+     * 图片的url
+     */
+    public String cover;
+
+    /**
+     * 跳转的url
+     */
+    public String link;
+
+    public String getCover(){
+        if (cover==null)
+            return "";
+        else
+            return cover;
+    }
+
+    public String getLink(){
+        if (link==null)
+            return "";
+        else
+            return link;
+    }
+
+}
---
+++ modules/core/app/utils/formatter/taozi/geo/SimpleLocalityFormatter.java
@@ -0,0 +1,66 @@
+package utils.formatter.taozi.geo;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.PropertyFilter;
+import com.fasterxml.jackson.databind.ser.PropertyWriter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import models.TravelPiBaseItem;
+import models.geo.Locality;
+import utils.formatter.JsonFormatter;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by lxf on 14-11-12.
+ */
+public class SimpleLocalityFormatter implements JsonFormatter {
+    @Override
+    public JsonNode format(TravelPiBaseItem item) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+
+        PropertyFilter localityFilter = new SimpleBeanPropertyFilter() {
+            @Override
+            public void serializeAsField
+                    (Object pojo, JsonGenerator jgen, SerializerProvider provider, PropertyWriter writer) throws Exception {
+                if (include(writer)) {
+                    writer.serializeAsField(pojo, jgen, provider);
+                } else if (!jgen.canOmitFields()) { // since 2.3
+                    writer.serializeAsOmittedField(pojo, jgen, provider);
+                }
+            }
+
+            private boolean includeImpl(PropertyWriter writer) {
+                Set<String> includedFields = new HashSet<>();
+                Collections.addAll(includedFields, Locality.fnZhName,Locality.simpId);
+
+                return (includedFields.contains(writer.getName()));
+            }
+
+            @Override
+            protected boolean include(BeanPropertyWriter beanPropertyWriter) {
+                return includeImpl(beanPropertyWriter);
+            }
+
+            @Override
+            protected boolean include(PropertyWriter writer) {
+                return includeImpl(writer);
+            }
+        };
+        FilterProvider filters = new SimpleFilterProvider().addFilter("localityFilter",localityFilter);
+        mapper.setFilters(filters);
+
+        return mapper.valueToTree(item);
+    }
+}
