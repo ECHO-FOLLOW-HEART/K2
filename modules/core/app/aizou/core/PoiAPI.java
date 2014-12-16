192b4a36726d9fbe941cb176562c35e851444002
linchuan <ppp21spider@sina.com>
2014-12-16 16:42:31 +0800

    MOD:目的地景点列表没显示location问题

--- modules/core/app/aizou/core/PoiAPI.java
+++ modules/core/app/aizou/core/PoiAPI.java
@@ -1040,7 +1040,7 @@ public class PoiAPI {
         Class<? extends AbstractPOI> poiClass = null;
         List<String> fieldList = new ArrayList<>();
         Collections.addAll(fieldList, "_id", "zhName", "enName", "rating", "images",
-                "desc", "locList", "priceDesc", "address", "tags");
+                "desc", "location", "priceDesc", "address", "tags");
         switch (poiType) {
             case VIEW_SPOT:
                 fieldList.add("timeCostDesc");
