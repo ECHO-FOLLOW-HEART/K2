15a91de725ad9f1f4c35c49ec8aefced80f8d323
海大富 <haizi.zh@gmail.com>
2014-12-22 19:54:45 +0800

    ADD: TDD / fetch input suggestions

--- test/test/travelpi/MiscTest.java
+++ test/test/travelpi/MiscTest.java
@@ -2,13 +2,10 @@ package test.travelpi;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import controllers.routes;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import play.GlobalSettings;
 import play.api.mvc.HandlerRef;
 import play.libs.Json;
 import play.mvc.Result;
-import play.test.FakeApplication;
 
 import java.util.regex.Pattern;
 
@@ -19,13 +16,6 @@ import static play.test.Helpers.*;
  * Created by zephyre on 12/21/14.
  */
 public class MiscTest extends TravelPiTest {
-
-    private static FakeApplication app;
-
-    @BeforeClass
-    public static void setup() {
-        app = fakeApplication(new GlobalSettings());
-    }
 
     /**
      * 获取首页图像
@@ -43,6 +33,53 @@ public class MiscTest extends TravelPiTest {
                 assertThat(node.get("code").asInt()).isEqualTo(0);
                 String imageUrl = node.get("result").get("image").asText();
                 assertThat(Pattern.compile("http://lxp-assets\\.qiniudn\\.com/app/").matcher(imageUrl).find()).isTrue();
+            }
+        });
+    }
+
+    /**
+     * 获得输入提示
+     */
+    @Test
+    public void suggestionCheck() {
+        running(app, new Runnable() {
+            public void checkEntry(JsonNode node) {
+                assertText(node, new String[]{"id", "_id", "name", "zhName"}, false);
+                assertText(node, new String[]{"enName", "fullName"}, true);
+            }
+
+            @Override
+            public void run() {
+                String word = "上海";
+                HandlerRef<?> handler = routes.ref.MiscCtrl.getSuggestions(word, true, true, false, false, 10);
+                Result result = callAction(handler);
+                JsonNode node = Json.parse(contentAsString(result));
+                assertThat(node.get("code").asInt()).isEqualTo(0);
+                node = node.get("result");
+
+                JsonNode locListNode = node.get("loc");
+                assertThat(locListNode.isArray());
+                assertThat(locListNode.size()).isGreaterThanOrEqualTo(1);
+                for (JsonNode locNode : locListNode) {
+                    checkEntry(locNode);
+                    JsonNode parent = locNode.get("parent");
+                    checkEntry(parent);
+                }
+
+                JsonNode vsListNode = node.get("vs");
+                for (JsonNode vsNode : vsListNode) {
+                    assertText(vsNode, new String[]{"_id", "name", "zhName"}, false);
+                    assertText(vsNode, "timeCost", true);
+                    assertThat(vsNode.get("imageList").isArray()).isTrue();
+
+                    JsonNode ratings = vsNode.get("ratings");
+                    for (String key : new String[]{"viewCnt", "favorCnt", "ranking", "checkinCnt"})
+                        assertThat(ratings.get(key).asDouble()).isGreaterThanOrEqualTo(0);
+
+                    JsonNode flags = vsNode.get("descriptionFlag");
+                    for (String key : new String[]{"desc", "traffic", "details", "tips"})
+                        assertThat(flags.get(key).asDouble()).isGreaterThanOrEqualTo(0);
+                }
             }
         });
     }
