26f5023c6374a05f7a0b25ca4b9a16372bb23e8d
海大富 <haizi.zh@gmail.com>
2014-12-22 19:46:08 +0800

    MOD: Add application initialization code to TravelPiTest.

--- test/test/travelpi/TravelPiTest.java
+++ test/test/travelpi/TravelPiTest.java
@@ -1,13 +1,25 @@
 package test.travelpi;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.BeforeClass;
+import play.GlobalSettings;
+import play.test.FakeApplication;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.fakeApplication;
 
 /**
  * Created by zephyre on 12/21/14.
  */
 public class TravelPiTest {
+
+    protected static FakeApplication app;
+
+    @BeforeClass
+    public static void setup() {
+        app = fakeApplication(new GlobalSettings());
+    }
+
     protected void assertText(JsonNode node, String field, boolean allowEmpty) {
         assertText(node, new String[]{field}, allowEmpty);
     }
