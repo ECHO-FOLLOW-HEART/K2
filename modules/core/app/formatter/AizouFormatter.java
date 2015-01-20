bcb05ca93409dc0701f2428231be8f4d83bb6fd7
海大富 <haizi.zh@gmail.com>
2015-01-20 16:36:13 +0800

    ADD: 改进序列化机制

---
+++ modules/core/app/formatter/AizouFormatter.java
@@ -0,0 +1,69 @@
+package formatter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.PropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import formatter.taozi.ObjectIdSerializer;
+import models.AizouBaseEntity;
+import models.geo.GeoJsonPoint;
+import org.bson.types.ObjectId;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zephyre on 1/20/15.
+ */
+public abstract class AizouFormatter<T extends AizouBaseEntity> {
+
+    public String format(List<T> itemList) throws JsonProcessingException {
+        return mapper.writeValueAsString(itemList);
+    }
+
+    public String format(T item) throws JsonProcessingException {
+        return mapper.writeValueAsString(item);
+    }
+
+    protected ObjectMapper mapper;
+
+    protected static Map<Class, AizouFormatter> instanceMap = new Hashtable<>();
+
+    protected ObjectMapper initObjectMapper(Map<String, PropertyFilter> filterMap,
+                                            Map<Class<? extends T>, JsonSerializer<T>> serializerMap) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        if (filterMap == null)
+            filterMap = new HashMap<>();
+
+        if (serializerMap == null)
+            serializerMap = new HashMap<>();
+
+        SimpleModule module = new SimpleModule();
+
+        // 添加ObjectId的序列化
+        module.addSerializer(ObjectId.class, new ObjectIdSerializer());
+
+        for (Map.Entry<Class<? extends T>, JsonSerializer<T>> entry : serializerMap.entrySet()) {
+            module.addSerializer(entry.getKey(), entry.getValue());
+        }
+
+        mapper.registerModule(module);
+
+        // 添加Location的序列化
+        SimpleFilterProvider filters = new SimpleFilterProvider()
+                .addFilter("geoJsonPointFilter", SimpleBeanPropertyFilter.filterOutAllExcept(GeoJsonPoint.FD_COORDS));
+
+        for (Map.Entry<String, PropertyFilter> entry : filterMap.entrySet())
+            filters.addFilter(entry.getKey(), entry.getValue());
+
+        mapper.setFilters(filters);
+
+        return mapper;
+    }
+}
