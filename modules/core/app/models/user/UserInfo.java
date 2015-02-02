1bee4a66c4379fc2ca7efa5487e66e5ab6a32dc6
林川 <ppp21spider@sina.com>
2015-02-02 17:27:51 +0800

    MOD: 显示头像时同时显示小图片URL。涉及：查看用户信息、搜索用户、好友列表。

--- modules/core/app/models/user/UserInfo.java
+++ modules/core/app/models/user/UserInfo.java
@@ -57,6 +57,9 @@ public class UserInfo extends AizouBaseEntity implements ITravelPiFormatter {
 
     @Transient
     public static String fnOauthId = "oauthList.oauthId";
+
+    @Transient
+    public static String fnAvatarSmall = "avatarSmall";
     /**
      * 昵称
      */
@@ -68,6 +71,13 @@ public class UserInfo extends AizouBaseEntity implements ITravelPiFormatter {
      */
     @JsonProperty("avatar")
     private String avatar;
+
+    /**
+     * 头像小图
+     */
+    @JsonProperty("avatarSmall")
+    private String avatarSmall;
+
     /**
      * 性别： F\M\Both\None
      */
@@ -263,6 +273,16 @@ public class UserInfo extends AizouBaseEntity implements ITravelPiFormatter {
 
     public void setOrigin(String origin) {
         this.origin = origin;
+    }
+
+    public String getAvatarSmall() {
+        if (avatar == null || avatar.equals(""))
+            return "";
+        return String.format("%s?imageView2/2/w/%d", avatar, 200);
+    }
+
+    public void setAvatarSmall(String avatarSmall) {
+        this.avatarSmall = avatarSmall;
     }
 
     @Override
