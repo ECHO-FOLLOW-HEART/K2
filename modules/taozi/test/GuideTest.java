3f59918438b24ea5447e4b555c151d4e4531bf70
Zephyre <haizi.zh@gmail.com>
2015-02-10 22:53:05 +0800

    ADD: 查看用户行程计划详情的测试代码

--- modules/taozi/test/GuideTest.java
+++ modules/taozi/test/GuideTest.java
@@ -8,6 +8,10 @@ import play.Configuration;
 import play.api.mvc.HandlerRef;
 import play.test.FakeApplication;
 import play.test.FakeRequest;
+import utils.validator.DoubleValidator;
+import utils.validator.IntegerValidator;
+import utils.validator.PositiveValidator;
+import utils.validator.RangeValidator;
 
 import java.io.File;
 
@@ -48,9 +52,96 @@ public class GuideTest {
                     assertFields(guide, "id", "images", "title", "updateTime", "dayCnt", "summary");
                     assertText(guide, false, "id", "title", "summary");
                     assertImages(guide.get("images"), true);
+
+                    assertNumber(guide.get("dayCnt"), false, new PositiveValidator(new IntegerValidator(), false));
+                    assertNumber(guide.get("updateTime"), false,
+                            new RangeValidator(new IntegerValidator(), 1e12, null, null));
+
                     for (String key : new String[]{"dayCnt", "updateTime"}) {
                         JsonNode val = guide.get(key);
                         assertThat(val.isIntegralNumber() && val.asLong() >= 0).isTrue();
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * 获得行程计划详情
+     */
+    @Test
+    public void testGuideDetails() {
+        running(app, new Runnable() {
+
+            private void checkPoi(JsonNode poi) {
+                assertFields(poi, "id", "zhName", "enName", "rating", "address", "images", "type", "timeCostDesc",
+                        "priceDesc", "rank", "targets", "locality", "location");
+                assertText(poi, false, "id", "zhName", "type");
+                assertText(poi, true, "enName", "address", "timeCostDesc", "priceDesc");
+
+                assertImages(poi.get("images"), true);
+                JsonNode targets = poi.get("targets");
+                assertThat(targets.isArray() && targets.size() > 0).isTrue();
+                for (JsonNode t : targets)
+                    assertThat(t.isTextual() && !t.asText().trim().isEmpty()).isTrue();
+                assertCoords(poi.get("location"));
+
+                assertNumber(poi.get("rating"), false,
+                        new RangeValidator(new DoubleValidator(), 0.0, 1.0, null));
+                assertNumber(poi.get("rank"), true,
+                        new PositiveValidator(new IntegerValidator(), true));
+
+                JsonNode locality = poi.get("locality");
+                if (!locality.isNull()) {
+                    assertText(locality, false, "id", "zhName");
+                    assertText(locality, true, "enName");
+                }
+            }
+
+            private void checkIter(JsonNode item) {
+                if (!item.isArray())
+                    System.out.println(item);
+                assertThat(item.isArray()).isTrue();
+
+                for (JsonNode entry : item) {
+                    assertFields(entry, "dayIndex", "poi");
+                    assertNumber(entry.get("dayIndex"), false,
+                            new PositiveValidator(new IntegerValidator(), false));
+                    checkPoi(entry.get("poi"));
+                }
+            }
+
+            @Override
+            public void run() {
+                String guideId = "54d361f11bf8194dbdcb9fbc";
+                for (String part : new String[]{"all", "itinerary", "shopping", "restaurant"}) {
+                    HandlerRef<?> handler = routes.ref.GuideCtrl.getGuideInfo(guideId, part);
+                    JsonNode node = getResultNode(handler);
+
+                    assertFields(node, "id", "images", "title", "userId", "itineraryDays", "updateTime", "localities",
+                            "itinerary", "shopping", "restaurant", "detailUrl");
+                    assertText(node, false, "id", "title", "detailUrl");
+                    assertImages(node.get("images"), true);
+
+                    assertNumber(node.get("userId"), false, new PositiveValidator(new IntegerValidator(), true));
+                    assertNumber(node.get("updateTime"), false,
+                            new RangeValidator(new IntegerValidator(), 1e12, null, null));
+                    assertNumber(node.get("itineraryDays"), false,
+                            new PositiveValidator(new IntegerValidator(), false));
+
+                    JsonNode locList = node.get("localities");
+                    assertThat(locList.isArray() && locList.size() > 0).isTrue();
+                    for (JsonNode loc : locList) {
+                        assertText(loc, false, "id", "zhName");
+                        assertText(loc, true, "enName");
+                    }
+
+                    switch (part) {
+                        case "itinerary":
+                            checkIter(node.get("itinerary"));
+                            break;
+                        default:
+                            break;
                     }
                 }
             }
