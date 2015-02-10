3b35c182a99e5ae20bc915b75669bc7988dd6c27
Zephyre <haizi.zh@gmail.com>
2015-02-10 15:22:59 +0800

    ADD: 查看城市详情和获得城市图集的测试代码

--- modules/taozi/test/GeoTest.java
+++ modules/taozi/test/GeoTest.java
@@ -90,52 +90,55 @@ public class GeoTest extends AizouTest {
         });
     }
 
-//    /**
-//     * 查看城市详情
-//     */
-//    @Test
-//    public void getLocality() {
-//        running(app, new Runnable() {
-//            @Override
-//            public void run() {
-//                String locId = "5473ccd7b8ce043a64108c46";
-//                int noteId = 0;
-//                HandlerRef<?> handler = routes.ref.GeoCtrl.getLocality(locId, noteId);
-//                Result result = callAction(handler);
-//                JsonNode node = Json.parse(contentAsString(result));
-//                assertThat(node.get("code").asInt()).isEqualTo(0);
-//                JsonNode response = node.get("result");
-//                JsonNode coords = response.get("location").get("coordinates");
-//                double lng = coords.get(0).asDouble();
-//                double lat = coords.get(1).asDouble();
-//                assertCoords(lng, lat);
-//                assertText(response, new String[]{"zhName", "desc", "id"}, false);
-//                assertText(response, new String[]{"travelMonth", "timeCostDesc"}, true);
-//                for (JsonNode tmp : response.get("images"))
-//                    assertThat(tmp.get("url").asText()).isNotEmpty();
-//            }
-//        });
-//    }
-//
-//    /**
-//     * 查看图集
-//     */
-//    @Test
-//    public void getAlbums() {
-//        running(app, new Runnable() {
-//            @Override
-//            public void run() {
-//                String locId = "547aebffb8ce043deccfed0b";
-//                int page = 0;
-//                int pageSize = 50;
-//                HandlerRef<?> handler = routes.ref.MiscCtrl.getAlbums(locId, page, pageSize);
-//                Result result = callAction(handler);
-//                JsonNode node = Json.parse(contentAsString(result));
-//                assertThat(node.get("code").asInt()).isEqualTo(0);
-//                JsonNode response = node.get("result");
-//                for (JsonNode tmp : response.get("album"))
-//                    assertThat(tmp.get("url").asText()).isNotEmpty();
-//            }
-//        });
-//    }
+    /**
+     * 查看城市详情
+     */
+    @Test
+    public void testGetLocality() {
+        running(app, new Runnable() {
+            @Override
+            public void run() {
+                String locId = "5473ccd7b8ce043a64108c46";
+                HandlerRef<?> handler = routes.ref.GeoCtrl.getLocality(locId);
+                JsonNode node = getResultNode(handler);
+
+                assertFields(node, "id", "zhName", "enName", "isFavorite", "desc", "timeCostDesc", "travelMonth",
+                        "imageCnt", "images", "location", "playGuide");
+                assertText(node, false, "id", "zhName", "desc", "timeCostDesc", "travelMonth", "playGuide");
+                assertText(node, true, "enName");
+
+                assertThat(node.get("isFavorite").isBoolean()).isTrue();
+                assertThat(node.get("imageCnt").asInt()).isPositive();
+
+                assertImages(node.get("images"), false);
+                assertCoords(node.get("location"));
+            }
+        });
+    }
+
+    /**
+     * 查看图集
+     */
+    @Test
+    public void getAlbums() {
+        running(app, new Runnable() {
+            @Override
+            public void run() {
+                String locId = "547aebffb8ce043deccfed0b";
+                int page = 0;
+                int pageSize = 50;
+                HandlerRef<?> handler = routes.ref.GeoCtrl.getLocalityAlbums(locId, page, pageSize);
+
+                JsonNode node = getResultNode(handler);
+                assertFields(node, "album", "albumCnt");
+
+                assertThat(node.get("albumCnt").asInt()).isPositive();
+
+                JsonNode album = node.get("album");
+                assertThat(album.isArray() && album.size() > 0).isTrue();
+                for (JsonNode image : album)
+                    assertText(image, false, "url", "originUrl");
+            }
+        });
+    }
 }
