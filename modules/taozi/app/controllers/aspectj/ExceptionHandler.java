42c144c20c1117206606189ad00b8232b305fec7
Zephyre <haizi.zh@gmail.com>
2015-02-12 20:54:08 +0800

    FIX: 异常处理的bug

--- modules/taozi/app/controllers/aspectj/ExceptionHandler.java
+++ modules/taozi/app/controllers/aspectj/ExceptionHandler.java
@@ -15,7 +15,7 @@ import utils.Utils;
 /**
  * Created by Heaven on 2014/12/30.
  */
-//@Aspect
+@Aspect
 public class ExceptionHandler {
     private Log logger = LogFactory.getLog(this.getClass());
 
@@ -28,28 +28,12 @@ public class ExceptionHandler {
         logger.info(String.format("Exception handler init: %s", debug));
     }
 
-//    @Around("execution(public play.mvc.Result controllers.taozi..*(..))")
-//    public Result catchException(ProceedingJoinPoint pjp) {
-//        try {
-//            return (Result) pjp.proceed();
-//        } catch (AizouException e) {
-//            if (debug)
-//                return Utils.createResponse(e.getErrCode(), e.getMessage());
-//            else
-//                return Utils.createResponse(e.getErrCode(), (String) null);
-//        } catch (Throwable throwable) {
-//            throwable.printStackTrace();
-//            logger.info("An unhandled Exception was caught by ExceptionHandler");
-//            logger.info("Action name:     " + pjp.getSignature());
-//            logger.info("CaughtException: " + throwable.toString());
-//            return Utils.createResponse(ErrorCode.UNKOWN_ERROR, throwable.toString());
-//        }
-//    }
 
-    @Around("@annotation(controllers.aspectj.catchException)")
-    public Object catchException1(ProceedingJoinPoint pjp) {
+
+    @Around("execution(public play.mvc.Result controllers.taozi..*(..)) || @annotation(controllers.aspectj.CatchException)")
+    public Result catchException(ProceedingJoinPoint pjp) {
         try {
-            return pjp.proceed();
+            return (Result) pjp.proceed();
         } catch (AizouException e) {
             if (debug)
                 return Utils.createResponse(e.getErrCode(), e.getMessage());
