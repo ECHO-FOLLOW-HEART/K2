952c51a63183354fbadd96ef6119e15e88ba8add
林川 <ppp21spider@sina.com>
2015-07-22 13:59:19 +0800

    MOD:保存行程单

--- modules/core/app/api/GuideAPI.scala
+++ modules/core/app/api/GuideAPI.scala
@@ -1,9 +1,13 @@
 package api
 
-import com.twitter.util.{ Future => TwitterFuture, FuturePool }
-import models.guide.{ AbstractGuide, Guide }
+import com.twitter.util.{Future => TwitterFuture, FuturePool}
+import exception.{AizouException, ErrorCode}
+import models.guide.{AbstractGuide, Guide, ItinerItem}
+import models.poi._
 import org.bson.types.ObjectId
 import org.mongodb.morphia.Datastore
+import org.mongodb.morphia.query.UpdateOperations
+import scala.collection.JavaConversions._
 
 import scala.language.postfixOps
 
@@ -11,6 +15,7 @@ import scala.language.postfixOps
  * Created by zephyre on 7/20/15.
  */
 object GuideAPI {
+
   object GuideStatus extends Enumeration {
     val planned = Value("planned")
     val traveled = Value("traveled")
@@ -55,4 +60,47 @@ object GuideAPI {
       ds.updateFirst(query, ops)
     }
   }
+
+  /**
+   * 更新行程单
+   *
+   * @param guideId
+   * @param guide
+   * @throws exception.AizouException
+   */
+  def updateGuide(guideId: String, guide: Guide, userId: Long)(implicit ds: Datastore, futurePool: FuturePool) = {
+    futurePool {
+      val query = ds.createQuery(classOf[Guide]) field "id" equal new ObjectId(guideId) field "userId" equal userId
+      if (query.iterator.hasNext) {
+        val update: UpdateOperations[Guide] = ds.createUpdateOperations(classOf[Guide])
+        if (guide.itinerary != null) {
+          fillPOIType(guide.itinerary)
+          update.set(AbstractGuide.fnItinerary, guide.itinerary)
+        }
+        if (guide.getItineraryDays != null) update.set(Guide.fnItineraryDays, guide.getItineraryDays)
+        if (guide.shopping != null) update.set(AbstractGuide.fnShopping, guide.shopping)
+        if (guide.restaurant != null) update.set(AbstractGuide.fnRestaurant, guide.restaurant)
+        if (guide.images != null) update.set(AbstractGuide.fnImages, guide.images)
+        //if (guide.localities != null) update.set(Guide.fnLocalities, guide.localities)
+        if (guide.getVisibility != null) update.set(Guide.fnVisibility, guide.getVisibility)
+        if (guide.getStatus != null) update.set(Guide.fnStatus, guide.getStatus)
+        if (guide.getUpdateTime != null) update.set(Guide.fnUpdateTime, System.currentTimeMillis)
+        ds.update(query, update)
+      }
+    }
+  }
+
+  def fillPOIType(itinerary: Seq[ItinerItem]): Unit = {
+    for (item <- itinerary) {
+      val poi = item.poi
+      poi match {
+        case ViewSpot => item.poi.`type` = "vs"
+        case Restaurant => item.poi.`type` = "restaurant"
+        case Shopping => item.poi.`type` = "shopping"
+        case Hotel => item.poi.`type` = "hotel"
+      }
+
+    }
+  }
+
 }
