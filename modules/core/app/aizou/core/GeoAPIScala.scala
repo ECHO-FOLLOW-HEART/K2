c4e15833777fe13a60f59aeb09a3e9427918f0f5
Zephyre <haizi.zh@gmail.com>
2015-07-29 19:35:42 +0800

    MOD: 动态更新国家图片

--- modules/core/app/aizou/core/GeoAPIScala.scala
+++ modules/core/app/aizou/core/GeoAPIScala.scala
@@ -1,10 +1,13 @@
 package aizou.core
 
+import java.util
+
 import com.twitter.util.{ Future, FuturePool }
 import models.geo.CountryExpert
 import org.mongodb.morphia.Datastore
 
 import scala.collection.JavaConversions._
+import scala.util.Random
 
 /**
  * Created by topy on 2015/7/23.
@@ -14,7 +17,14 @@ object GeoAPIScala {
   def getCountryExperts()(implicit ds: Datastore, futurePool: FuturePool): Future[Seq[CountryExpert]] = {
     futurePool {
       val query = ds.createQuery(classOf[CountryExpert])
-      query.asList()
+      query.asList() map (c => {
+        val images = c.getImages
+        if (images != null && images.size() > 1) {
+          val idx = Random.nextInt(images.size())
+          c.setImages(util.Arrays.asList(images.get(idx)))
+        }
+        c
+      })
     }
   }
 
