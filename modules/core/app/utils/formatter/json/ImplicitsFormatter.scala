e5abe017687726f447bf6f1344bf64b124b63b10
林川 <ppp21spider@sina.com>
2015-08-05 14:33:39 +0800

    MOD:添加上传通讯录接口

--- modules/core/app/utils/formatter/json/ImplicitsFormatter.scala
+++ modules/core/app/utils/formatter/json/ImplicitsFormatter.scala
@@ -1,6 +1,7 @@
 package utils.formatter.json
 
 import models.geo.{ Locality => K2Locality }
+import models.user.{ Contact => K2Contact }
 import org.bson.types.ObjectId
 import org.mongodb.morphia.annotations.Entity
 import play.api.libs.json.Json
@@ -14,7 +15,24 @@ object ImplicitsFormatter {
   @Entity(noClassnameStored = true)
   case class Locality(id: String, zhName: String, enName: String)
 
+  case class JsonContact(entryId: Int, sourceId: Int, name: String, tel: String)
+
   implicit val localityReads = Json.reads[Locality]
+  implicit val contactReads = Json.reads[JsonContact]
+
+  implicit def contact2Model(contact: JsonContact): K2Contact = {
+    val l: K2Contact = new K2Contact()
+    l.setId(new ObjectId())
+    l.setEntryId(contact.entryId)
+    l.setSourceId(contact.sourceId)
+    l.setName(contact.name)
+    l.setTel(contact.tel)
+    l
+  }
+
+  implicit def contact2Model(contacts: Seq[JsonContact]): java.util.List[K2Contact] = {
+    contacts.map(contact2Model(_)).asJava
+  }
 
   implicit def locality2Model(locality: Locality): K2Locality = {
     val l: K2Locality = new K2Locality()
