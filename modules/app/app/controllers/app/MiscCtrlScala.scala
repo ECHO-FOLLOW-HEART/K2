45fceea4925e1707cc9b21d3f00dc93706f7080d
ECHO-FOLLOW-HEART <pengyt19890703@163.com>
2015-08-05 18:18:42 +0800

    about/eula

--- modules/app/app/controllers/app/MiscCtrlScala.scala
+++ modules/app/app/controllers/app/MiscCtrlScala.scala
@@ -19,43 +19,11 @@ object MiscCtrlScala extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-
-  def comments(id: String) = Action {
-    val results = MiscAPI.getComments[RestaurantComment](classOf[RestaurantComment], new ObjectId(id), 0.0, 1.0, 0, 0, 15)
-    Ok(views.html.comments(id, 1, 20, "restaurant", results))
-  }
-  def displayComment(poiType: String, poiId: String, lower: Double, upper: Double, lastUpdate: Long, page: Int, pageSize: Int, snippets: Boolean) = Action {
-    request =>
-      {
-        val formatter = poiType match {
-          case "restaurant" => FormatterFactory.getInstance(classOf[CommentFormatter])
-          case _ => throw new AizouException(ErrorCode.INVALID_ARGUMENT, String.format("Invalid POI type: %s.", poiType))
-        }
-
-        // 判断请求是何种格式
-        val reqDataFormat = request.headers.get("Accept").get.toString
-        if (reqDataFormat.contains("application/json")) {
-          val commentList = MiscAPI.getComments[RestaurantComment](classOf[RestaurantComment], new ObjectId(poiId), lower, upper, lastUpdate, page, pageSize)
-          val results = formatter.formatNode((Option(commentList) map (_.toSeq)).get)
-          Utils.createResponse(ErrorCode.NORMAL, results).toScala
-        } else if (reqDataFormat.contains("text/html")) {
-          if (snippets) {
-            val results = MiscAPI.getComments[RestaurantComment](classOf[RestaurantComment], new ObjectId(poiId), lower, upper, lastUpdate, page, pageSize)
-            Ok(views.html.comments(poiId, page, pageSize, poiId, results))
-          } else {
-            val results = MiscAPI.getComments[RestaurantComment](classOf[RestaurantComment], new ObjectId(poiId), lower, upper, lastUpdate, page, pageSize)
-            Ok(views.html.commentsEntry(poiId, page, pageSize, poiId, results))
-          }
-        } else {
-          Utils.createResponse(ErrorCode.INVALID_ARGUMENT, String.format("Invalid http accept type: %s.", reqDataFormat)).toScala
-        }
-      }
-  }
-
   // 协议
   def eula() = Action {
     Ok(views.html.eula())
   }
+
   //公司介绍
   def about(version: String) = Action {
     Ok(views.html.about(version))
