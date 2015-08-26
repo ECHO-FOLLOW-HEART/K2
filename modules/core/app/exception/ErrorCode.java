fa686037aac9b11a05c95e0439baf2695c99bd56
林川 <ppp21spider@sina.com>
2015-08-26 19:16:37 +0800

    MOD:定义ErrorCode

--- modules/core/app/exception/ErrorCode.java
+++ modules/core/app/exception/ErrorCode.java
@@ -10,7 +10,11 @@ public enum ErrorCode {
      * 正常
      */
     NORMAL(0),
-
+    /**
+     * 未知错误
+     */
+    UNKOWN_ERROR(900),
+    /** K2 系统性 */
     /**
      * 输入参数错误
      */
@@ -23,16 +27,34 @@ public enum ErrorCode {
      * 数据库错误
      */
     DATABASE_ERROR(102),
-
+    /**
+     * 搜索引擎通用错误
+     */
+    SEARCH_ENGINE_ERROR(103),
+    /**
+     * Yunkai工程中的Exception对应的ErrorCode
+     */
+    YUNKAI_NOTFOUND(200),
+    YUNKAI_INVALIDARGS(201),
+    YUNKAI_AUTH(202),
+    YUNKAI_USEREXISTS(203),
+    YUNKAI_RESOURCECONFLICT(204),
+    YUNKAI_GROUPMEMBERSLIMIT(205),
+    YUNKAI_INVALIDSTATE(206),
+    YUNKAI_VALIDATIONCODE(207),
+    YUNKAI_OVERQUOTALIMIT(208),
+    /**
+     * Hedylogs工程中的Exception对应的ErrorCode
+     */
+    HEDY_BASE_EXP(300),
+    HEDY_BLACKLIST_EXP(301),
+    HEDY_CONTACT_EXP(302),
+    HEDY_GROUPMEMBER_EXP(303),
+    HEDY_REDIS_EXP(304),
     /**
      * 数据已存在的错误
      */
     DATA_EXIST(401),
-
-    /**
-     * 数据不存在的错误
-     */
-    DATA_NOT_EXIST(402),
 
     /**
      * 注册时验证码错误
@@ -40,9 +62,10 @@ public enum ErrorCode {
     CAPTCHA_ERROR(403),
 
     /**
-     * 微信注册时请求用户信息失败
+     * 数据不存在的错误
      */
-    WEIXIN_CODE_ERROR(404),
+    DATA_NOT_EXIST(404),
+
     /**
      * 用户已存在
      */
@@ -61,23 +84,12 @@ public enum ErrorCode {
     /**
      * 短信发送超过配额
      */
-    SMS_QUOTA_ERROR(500),
+    SMS_QUOTA_ERROR(410),
 
     /**
      * 无效的短信操作码
      */
-    SMS_INVALID_ACTION(501),
-
-    /**
-     * 搜索引擎通用错误
-     */
-    SEARCH_ENGINE_ERROR(600),
-
-    /**
-     * 未知错误
-     */
-    UNKOWN_ERROR(900);
-
+    SMS_INVALID_ACTION(411);
     private final int val;
 
     ErrorCode(int val) {
