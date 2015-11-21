c3e49eccac54714b225cffeba399bc2d0f6a6237
topy <ppp21spider@sina.com>
2015-11-21 11:10:56 +0800

    MOD:添加对Hanse接口的转发

---
+++ modules/app/app/controllers/app/HanseMiscCtrlScala.scala
@@ -0,0 +1,40 @@
+package controllers.app
+
+import play.api.Play.current
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.ws.{ WS, WSRequest, WSResponse }
+import play.api.mvc.{ Action, ResponseHeader, Result }
+import scala.concurrent.{ ExecutionContext, Future }
+import ExecutionContext.Implicits.global
+
+/**
+ * Created by topy on 2015/11/21.
+ */
+object HanseMiscCtrlScala {
+
+  def columns() = redirects("http://192.168.100.3:9480/columns")
+
+  def recommend() = redirects("http://192.168.100.3:9480/recommend")
+
+  def commoditiesByType(topicType: String) = redirects("http://192.168.100.3:9480/columns/commodities")
+
+  def commoditiesById(id: String) = redirects("http://192.168.100.3:9480/commodities/" + id)
+
+  def redirects(url: String) = Action.async(
+    requestIn => {
+      val requestOut: WSRequest = WS.url(url)
+      val queryString = requestIn.queryString.map { case (k, v) => k -> v.mkString }
+      Response2Result(requestOut.withQueryString(queryString.toList: _*).get())
+    }
+  )
+
+  def Response2Result(response: Future[WSResponse]): Future[Result] = {
+    response map {
+      response =>
+        Result(ResponseHeader(response.status, response.allHeaders map {
+          h => (h._1, h._2.head)
+        }), Enumerator(response.body.getBytes))
+    }
+  }
+
+}
